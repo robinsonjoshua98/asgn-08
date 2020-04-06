9ed1c038bc2e3d8d3b0dbee9679ee960a47ec6ae
Joshua Robinson <robinsonjoshua98@gmail.com>
2020-04-06 10:20:33 -0400

    Added database/connection.php file

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$server = "localhost";
-$user = "wbip";
-$pw = "wbip123";
-$db = "test";
---
+++ database/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+if($_SERVER['HTTP_HOST'] == "joshuarobinson.com") 
+
+
+{
+    
+define('SERVER' , 'localhost');
+define('USER' , 'joshua99_wbip');
+define('PW' , 'Davidrob617!');
+define('DB' , 'joshua99_test');
+
+    
+}
+
+    else {
+define('SERVER' , 'localhost');
+define('USER' , 'wbip');
+define('PW' , 'wbip123');
+define('DB' , 'test');
+}
